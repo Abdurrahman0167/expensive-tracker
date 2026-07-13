7bd2816b2b650ff4e08e9421d9db7678c058af99
Abdurrahman <96380888+Abdurrahman0167@users.noreply.github.com>
2026-07-13 15:37:02 +0300

    initialize DOM elements

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
-"use strict"
+"use strict";
 
+const actualBalance = document.querySelector(".actual-balance");
+const transictionIncome = document.querySelector(".balance-income");
+const transictionExcome = document.querySelector(".balance-excome");
+const transactionName = document.querySelector(".transaktion-name__input");
+const transactionSumm = document.querySelector(".transaktion-money__money");
+const transictionButton = document.querySelector(".transaktion-button");
+const transictionHistory = document.querySelector(".history");
