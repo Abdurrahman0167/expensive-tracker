8cbd1d2c82184278d6f9fbd478272056ebc1e0be
Abdurrahman <96380888+Abdurrahman0167@users.noreply.github.com>
2026-07-14 11:53:24 +0300

    feat: handle click event for transition button

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ const actualBalance = document.querySelector(".actual-balance");
 const transictionIncome = document.querySelector(".balance-income");
 const transictionExcome = document.querySelector(".balance-excome");
 const transactionName = document.querySelector(".transaktion-name__input");
-const transactionSumm = document.querySelector(".transaktion-money__money");
+const transactionSum = document.querySelector(".transaktion-money__money");
 const transictionButton = document.querySelector(".transaktion-button");
 const transictionHistory = document.querySelector(".history");
+
+transictionButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log(transactionName.value, transactionSum.value);
+
+  transactionName.value = "";
+  transactionSum.value = "";
+
+});
+
