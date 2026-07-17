db6469d7be3c0d717be9c37e267caea1038cccab
Abdurrahman <96380888+Abdurrahman0167@users.noreply.github.com>
2026-07-17 16:43:14 +0300

    add a balance deduction button via querySelector

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const transictionExcome = document.querySelector(".balance-excome");
 const transactionName = document.querySelector(".transaktion-name__input");
 const transactionSum = document.querySelector(".transaktion-money__money");
 const addMoneyBtn = document.querySelector(".transaktion-button-plus");
-
+const subtractMoneyBtn = document.querySelector('."transaktion-button-minus')
 const transictionHistory = document.querySelector(".history");
 
 transictionButton.addEventListener("click", (event) => {
