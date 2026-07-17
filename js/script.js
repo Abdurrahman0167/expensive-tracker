10605f8e82b9bf24775cd561b4136194afc21636
Abdurrahman <96380888+Abdurrahman0167@users.noreply.github.com>
2026-07-17 15:55:44 +0300

    style: rename transaktion button class

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const transictionIncome = document.querySelector(".balance-income");
 const transictionExcome = document.querySelector(".balance-excome");
 const transactionName = document.querySelector(".transaktion-name__input");
 const transactionSum = document.querySelector(".transaktion-money__money");
-const transictionButton = document.querySelector(".transaktion-button");
+const transictionButton = document.querySelector(".transaktion-button-plus");
 const transictionHistory = document.querySelector(".history");
 
 transictionButton.addEventListener("click", (event) => {
