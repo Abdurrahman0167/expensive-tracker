39f04ae4a94e2e125ea26a2f3825edbbfe81b2d4
Abdurrahman <96380888+Abdurrahman0167@users.noreply.github.com>
2026-07-19 09:55:46 +0300

    feat: add 'add money' function

--- js/script.js
+++ js/script.js
@@ -6,6 +6,20 @@ const transictionExcome = document.querySelector(".balance-excome");
 const transactionName = document.querySelector(".transaktion-name__input");
 const transactionSum = document.querySelector(".transaktion-money__money");
 const addMoneyBtn = document.querySelector(".transaktion-button-plus");
-const subtractMoneyBtn = document.querySelector('."transaktion-button-minus')
+const subtractMoneyBtn = document.querySelector(".transaktion-button-minus");
 const transictionHistory = document.querySelector(".history");
+const balance = document.querySelector(".my-balance");
 
+function addMoney() {
+  addMoneyBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    let value = Number(balance.textContent.replace("₽", ""));
+    let sum = Number(transactionSum.value);
+    value += sum;
+    balance.textContent = value + "₽";
+
+    transactionName.value =''
+    transactionSum.value =''
+  });
+}
